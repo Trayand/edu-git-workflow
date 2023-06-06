e5fb6a46a069e13d8539397ef5c34194ee442115
Tray-PC <patradyn@gmail.com>
2023-06-06 10:01:44 +0700

    Feat: cms register

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Controller = require('./controllers')
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
@@ -6,5 +7,7 @@ app.use(express.urlencoded({extended: true}))
 app.get("/", (request, response, next) => {
     response.json({message: "Aman gan"})
 })
+
+app.get("/register", Controller.register)
 
 app.listen(3000, () => console.log("Jalan kok gan"))
