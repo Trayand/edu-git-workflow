891312b674dcf50f4b7f55832c43dd18184d5dd3
Tray-PC <patradyn@gmail.com>
2023-06-06 09:49:58 +0700

    add skeleton code
    - app
    - controllers/index

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (request, response) => {
+    response.json({message: "Aman gan"})
+})
+
+app.listen(3000, () => console.log("Jalan kok gan"))
